4b1d38f3d9ef92e6937d438953fc816e5031b745
Vinicius de Sa Souza <vinicius@programador.eti.br>
2018-08-05 12:53:42 -0300

    remove contentType middleware

--- src/app.js
+++ src/app.js
@@ -4,14 +4,12 @@ import bodyParser from 'body-parser';
 import notificationsRoutes from './routes/notifications';
 import notificationsMiddleware from "./middlewares/notifications";
 import cors from "./middlewares/cors";
-import contentType from "./middlewares/contentType";
 import eventsRoutes from "./routes/events";
 import eventsMiddleware from "./middlewares/events";
 
 export default express()
     .use(bodyParser.json())
     .use(cors)
-    .use(contentType)
     .use('/notifications', notificationsMiddleware)
     .use('/notifications', notificationsRoutes)
     .use('/events', eventsMiddleware)
